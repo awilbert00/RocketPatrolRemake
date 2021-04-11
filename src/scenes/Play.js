f31fe4b53e9b8679c4fb0dd273dcf0cd28112d82
Alexa Wilbert <awilbert@ucsc.edu>
2021-04-10 17:11:57 -0700

    Added end/replay screen

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -97,19 +97,35 @@ class Play extends Phaser.Scene {
             this.p1Score,
             scoreConfig);
 
+        this.gameOver = false
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(10000, () => {
+            this.add.text(game.config.width/2, game.config.height/2,
+                'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2+64,
+                'Press (R) to restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
+
 
     }
 
     update() {
         this.starfield.tilePositionX -= 2;
-        this.p1Rocket.update();
-        this.ship1.update();
-        this.ship2.update();
-        this.ship3.update();
+        if (!this.gameOver) {
+            this.p1Rocket.update();
+            this.ship1.update();
+            this.ship2.update();
+            this.ship3.update();
+        }
 
         this.checkCollision(this.p1Rocket, this.ship1);
         this.checkCollision(this.p1Rocket, this.ship2);
         this.checkCollision(this.p1Rocket, this.ship3);
+
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
 
     }
 
