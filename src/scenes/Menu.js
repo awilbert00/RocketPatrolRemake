1a7bff11a079f18785c5591bd241e1d2840a15e2
Alexa Wilbert <awilbert@ucsc.edu>
2021-04-10 18:06:51 -0700

    Added difficulty modes, adjusted timers and speed

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,6 +2,13 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    preload() {
+        this.load.audio('sfx_select', 'assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', 'assets/explosion38.wav');
+        this.load.audio('sfx_rocket', 'assets/rocket_shot.wav');
+
+   }
     create() {
         let menuConfig = {
             fontFamily: 'Courier',
@@ -24,6 +31,33 @@ class Menu extends Phaser.Scene {
         menuConfig.color = "#000";
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding,
             'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
+
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            //easy mode
+            game.settings = {
+                spaceshipSpeed: 3,
+                gameTimer: 2000
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            //hard mode
+            game.settings = {
+                spaceshipSpeed: 4,
+                gameTimer: 10000
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');
+        }
+
+        
     }
 }
